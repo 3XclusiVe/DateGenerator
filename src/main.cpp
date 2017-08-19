97f9993a12332678ba971a23e9637a831eec951d
Dmitriy Malych <malllkin007@gmail.com>
2017-08-19 23:36:28 +0500

    added test for checking that date is not a weekend

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,26 @@
 #include "Date.h"
 #include "TestDateGenerator.h"
 
+bool isWeekEnd(std::string &date) {
+    int day, month, year;
+    sscanf(date.c_str(), "%2d.%2d.%4d",
+           &day,
+           &month,
+           &year);
+
+    static int t[] = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
+    year -= month < 3;
+    int weekDay = ( year + year/4 - year/100 + year/400 + t[month-1] + day) % 7;
+
+    return ((weekDay == 0) || (weekDay == 6));
+}
 
 
 int main() {
+
+    //Как использовать
+    Date DateGenerator = Date();
+    std::string date = DateGenerator.Generate();
 
     //Тесты
     TestDateGenerator testDateGenerator = TestDateGenerator();
@@ -17,10 +34,11 @@ int main() {
     while (true) {
         //Генерация случайной даты
         Date DateGenerator = Date();
-        std::string date = DateGenerator.Generate();
+        date = DateGenerator.Generate();
 
-        //Вывод на экран и пауза
-        std::cout << date << std::endl;
+        //Вывод на экран строки даты и проверки являюется ли дата выходным (только тест)
+        std::cout << date  << " "<< isWeekEnd(date) << std::endl;
+        //пауза 500 мс
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
 
