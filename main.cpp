e17d940d55282c43aafe44d864721e0411bd9887
Dmitriy Malych <malllkin007@gmail.com>
2017-08-19 16:03:11 +0500

    fast test added

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <random>
+#include <thread>
 #include "Date.h"
 
 using namespace std;
 
 int main() {
 
-    Date DateGenerator = Date();
-    std::string date = DateGenerator.Generate();
+    while (true) {
+        Date DateGenerator = Date();
+        std::string date = DateGenerator.Generate();
 
-    cout << date << std::endl;
+        cout << date << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
 
     return 0;
 }
