ad7f5ce634528e90b8cdf54739a5c97a274ff8d6
Dmitriy Malych <malllkin007@gmail.com>
2017-08-20 21:55:57 +0500

    added comments

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,18 @@ using namespace std;
 
 int main() {
 
+    //Тесты
     TestDateGenerator testDateGenerator = TestDateGenerator();
     testDateGenerator.TestMaxDayInFebruary();
     testDateGenerator.TestMaxDayInFebruaryWhenYearIsLeap();
     testDateGenerator.TestMaxDayinMonths();
 
     while (true) {
+        //Генерация случайной даты
         Date DateGenerator = Date();
         std::string date = DateGenerator.Generate();
 
+        //Вывод на экран и пауза
         cout << date << std::endl;
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
